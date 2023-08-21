7440c30d377a9764b6dea3a098def310205b0a78
viniciusventura29 <viniciusventura29@gmail.com>
2023-08-21 02:09:36 -0300

    add players endpoint (with errors)

--- routes/routes.go
+++ routes/routes.go
@@ -16,15 +16,34 @@ func AppRouter(Router *gin.Engine, client *db.PrismaClient) *gin.RouterGroup {
 	{
 		v1.GET("/", func(c *gin.Context) {
 
-			posts, err := client.Post.FindMany().Exec(ctx)
+			player, err := client.Player.FindMany().Exec(ctx)
 
 			if err != nil {
 				return
 			}
 
 			c.JSON(http.StatusOK, gin.H{
-				"message": posts,
+				"message": player,
 			})
+		})
+
+		v1.POST("/addPlayer", func(c *gin.Context) {
+
+			playerInfo := c.Request.Body
+
+			player := client.Player.CreateOne(
+				db.Player.Nickname.Set(playerInfo.nickname),
+				db.Player.Image.Set(""),
+				db.Player.Country.Set(playerInfo.country),
+				db.Player.Points.set(playerInfo.points),
+				db.Player.LastPoints.Set("70"),
+				db.Player.Name.Set(playerInfo.name),
+				db.Player.LastName.Set(playerInfo.lastName),
+				db.Player.Lane.Set(playerInfo.lane),
+				db.Player.LaneImg.Set(""),
+				db.Player.Team.Link(nil),
+			)
+
 		})
 	}
 
