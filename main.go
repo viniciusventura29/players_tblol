7a7d9fa47e9e3c27e0d23be5507121e5fc23eeca
viniciusventura29 <viniciusventura29@gmail.com>
2023-08-24 12:08:48 -0300

    new cors

--- main.go
+++ main.go
@@ -4,11 +4,18 @@ import (
 	"players_tblol/db"
 	"players_tblol/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	app := gin.Default()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173/", "http://localhost:3000/", "http://localhost:4000/"},
+		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "DELETE"},
+		AllowCredentials: true,
+	}))
 
 	client := db.NewClient()
 	if err := client.Prisma.Connect(); err != nil {
